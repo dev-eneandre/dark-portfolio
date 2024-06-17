4f13137c17c434f6dc96a59d748598873a1625a1
DevAndre <iameneandre@gmail.com>
2024-06-17 11:11:11 +0100

    localhost path change

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -2,7 +2,9 @@ import { Social } from "@/typings";
 
 export const fetchSocial = async () => {
   // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
-  const res = await fetch(`http://localhost:3000/api/getSocials`);
+  const res = await fetch(
+    `https://dark-portfolio-ij19hvssr-deveneandres-projects.vercel.app/api/getSocials`
+  );
 
   const data = await res.json();
   const socials: Social[] = data.socials;
