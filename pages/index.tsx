881531acfc68e5e738da3b8d194c4996ebfba311
DevAndre <iameneandre@gmail.com>
2024-06-20 16:49:38 +0100

    vercel trial

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,49 +14,56 @@ import { fetchExperiences } from "@/utils/fetchExperiences";
 import { fetchSkill } from "@/utils/fetchSkills";
 import { fetchProjects } from "@/utils/fetchProjects";
 import { fetchSocial } from "@/utils/fetchSocials";
+import { useEffect } from "react";
 
 type Props = {
   pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
-  socials: Social[];
+  // experiences: Experience[];
+  // skills: Skill[];
+  // projects: Project[];
+  // socials: Social[];
 };
 export default function Home({
   pageInfo,
-  experiences,
-  skills,
-  projects,
-  socials,
+  // experiences,
+  // skills,
+  // projects,
+  // socials,
 }: Props) {
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_BASE_URL);
+    console.log("what a life i live");
+  }, []);
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#a84b4b]/80">
       <Head>
         <title>Next Js Portfolio</title>
       </Head>
 
-      <Header socials={socials} />
+      {/* <Header socials={socials} /> */}
 
       {/* Hero section  */}
-
       <section id="hero" className="snap-start">
         <Hero pageInfo={pageInfo} />
       </section>
-      <section id="about" className="snap-center">
-        <About pageInfo={pageInfo} />
-      </section>
-      <section id="experience" className="snap-center">
-        <WorkExperience experiences={experiences} />
-      </section>
-      <section id="skills" className="snap-start">
-        <Skills skills={skills} />
-      </section>
-      <section id="projects" className="snap-start">
-        <Projects projects={projects} />
-      </section>
-      <section id="contact" className="snap-start">
-        <ContactMe />
-      </section>
+
+      {/* <div>
+        <section id="about" className="snap-center">
+          <About pageInfo={pageInfo} />
+        </section>
+        <section id="experience" className="snap-center">
+          <WorkExperience experiences={experiences} />
+        </section>
+        <section id="skills" className="snap-start">
+          <Skills skills={skills} />
+        </section>
+        <section id="projects" className="snap-start">
+          <Projects projects={projects} />
+        </section>
+        <section id="contact" className="snap-start">
+          <ContactMe />
+        </section>
+      </div> */}
 
       <Link href="#hero">
         <footer className="sticky bottom-5 w-full cursor-pointer">
@@ -73,20 +80,20 @@ export default function Home({
   );
 }
 
-  export const getStaticProps: GetStaticProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperiences();
-  const skills: Skill[] = await fetchSkill();
-  const projects: Project[] = await fetchProjects();
-  const socials: Social[] = await fetchSocial();
+  // const experiences: Experience[] = await fetchExperiences();
+  // const skills: Skill[] = await fetchSkill();
+  // const projects: Project[] = await fetchProjects();
+  // const socials: Social[] = await fetchSocial();
 
   return {
     props: {
       pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
+      // experiences,
+      // skills,
+      // projects,
+      // socials,
     },
     revalidate: 10,
   };
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,15 +1,10 @@
 import { PageInfo } from "@/typings";
 
 export const fetchPageInfo = async () => {
-  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
-  const res = await fetch(
-    `https://dark-portfolio-git-main-deveneandres-projects.vercel.app/api/getPageInfo`
-  );
-
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const res = await fetch(`${baseUrl}/api/getPageInfo`);
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
-
-  //   console.log("fetching ", pageInfo);
 
   return pageInfo;
 };
