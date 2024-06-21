98ad46e5ea508c281de66d5f137235c430523567
DevAndre <iameneandre@gmail.com>
2024-06-21 23:48:51 +0100

    build issues

--- sanity.ts
+++ sanity.ts
@@ -3,11 +3,17 @@ import createImageUrlBuilder from "@sanity/image-url";
 // import { apiVersion, dataset, projectId, useCdn } from "./sanity/env";
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: "production",
+  projectId: "https://vercel.com/deveneandres-projects/dark-portfolio",
   apiVersion: "2024-06-14",
   useCdn: process.env.NODE_ENV === "production",
 };
+// export const config = {
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   apiVersion: "2024-06-14",
+//   useCdn: process.env.NODE_ENV === "production",
+// };
 
 export const sanityClient = createClient(config);
 
