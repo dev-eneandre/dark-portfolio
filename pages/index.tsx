f07b06ff727cfcd91b8c5297bf76391787e0b9db
DevAndre <iameneandre@gmail.com>
2024-06-22 14:19:45 +0100

    build trial3

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,10 +30,7 @@ export default function Home({
   projects,
   socials,
 }: Props) {
-  useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_BASE_URL);
-    console.log("what a life i live");
-  }, []);
+
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#a84b4b]/80">
       <Head>
