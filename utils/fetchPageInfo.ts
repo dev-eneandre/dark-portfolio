4c4bced6b8e2ca9f2cc330d704973123d3441b77
DevAndre <iameneandre@gmail.com>
2024-06-22 17:24:26 +0100

    urls updated

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,7 +3,8 @@ import { PageInfo } from "@/typings";
 export const fetchPageInfo = async () => {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   // const res = await fetch(`http://localhost:3000/api/getPageInfo`);
-  const res = await fetch(`${baseUrl}/api/getPageInfo`);
+  // const res = await fetch(`${baseUrl}/api/getPageInfo`);
+  const res = await fetch(`/api/getPageInfo`);
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
 
