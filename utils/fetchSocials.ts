84f229185812ee007205691fe7acf73785822223
DevAndre <iameneandre@gmail.com>
2024-06-22 17:42:05 +0100

    urls updated 3

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -2,12 +2,12 @@ import { Social } from "@/typings";
 
 export const fetchSocial = async () => {
   // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
-  // const res = await fetch(`http://localhost:3000/api/getSocials`);
-  const res = await fetch(`api/getSocials`);
+  const res = await fetch(`http://localhost:3000/api/getSocials`);
 
   const data = await res.json();
   const socials: Social[] = data.socials;
 
   //   console.log("fetching ", socials);
+
   return socials;
 };
